10db9e1fe929fccc828cb9b28813fb903793999d
davidbanu <davidbanu@ymail.com>
2018-07-27 21:39:34 +0300

    Change in ajax request added dataType, contentType and cross domain

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,9 +20,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                // url: "././mail/contact_me.php",
-                url: "mail/contact_me.php",
+                url: "././mail/contact_me.php",
                 type: "POST",
+                contentType: "application/json",
+                crossDomain: true, 
+                dataType: "json",
                 data: {
                     name: name,
                     email: email,
