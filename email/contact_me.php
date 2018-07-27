b79fcf1bec712b033f01a63f248d174acefffe3c
davidbanu <davidbanu@ymail.com>
2018-07-27 17:49:37 +0300

    Send email
    Send email with infos to specific address

---
+++ email/contact_me.php
@@ -0,0 +1,28 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['service'])     ||
+   empty($_POST['budget']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$service = $_POST['service'];
+$budget = $_POST['budget'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'banudavidcip@gmail.com'; 
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nService: $service\n\nBudget: $budget\n\nMessage:\n$message";
+$headers = "From: noreply@davidbanu.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
