874d14f227f84ddadf16a2918f9858c32b3a232b
davidbanu <davidbanu@ymail.com>
2018-07-30 22:52:08 +0300

    Removing unecessary file

--- email/contact_me.php
+++ email/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -6,7 +7,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['budget']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+    {
 	echo "No arguments Provided!";
 	return false;
    }
